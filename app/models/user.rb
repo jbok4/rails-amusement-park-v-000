180433cd95b195ffc5e1b16e418b2a25787bbd1e
jbok4 <jbok4@aol.com>
2016-09-25 22:34:27 -0400

    user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  # write associations here
+  validates :name, presence: true
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, :through => :rides
+
+  def mood
+    if self.nausea <= self.happiness
+      "happy"
+    else
+      "sad"
+    end
+  end
+
 end
