f9ba3899f4b2131dca04ca8b3b25118d57cfd77e
jbok4 <jbok4@aol.com>
2016-09-25 22:40:10 -0400

    take_ride method begun

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,10 @@
 class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
+
+  def take_ride
+    if user.tickets >= attractions.tickets && user.height >= attraction.height
+  end
+
+
 end
